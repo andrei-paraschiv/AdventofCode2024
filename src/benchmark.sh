60d066881b672a4df33a500c938eeb67d8b405f8
Andrei Paraschiv <aparasch@uwaterloo.ca>
2024-12-09 01:26:26 -0500

    improved benchmark.sh

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
 make all
-echo "Benchmarking all days, avg and best times of 100 repetitions in microseconds:"
-echo -e "\t\t\tAverage \tBest"\
+echo -e "Benchmarking all days, avg and best times of 100 repetitions in microseconds:"
+echo -ne "Progress: Day 01"
+echo "Day;Part;Average;Best"\ > tmp2.txt
 
 numFiles=$(ls -1q day?? | wc -l)
 for i in $(seq -f "%02g" 1 $numFiles);
 do
-    echo "Day $i:"
+    echo -ne "\b\b$i"
+    echo "Day $i:" >> tmp2.txt
     part1best=0
     part2best=0
     part1avg=0
     part2avg=0
     for j in $(seq 1 100);
     do
-        ./day$i ../input/day$i.txt > ./tmp.txt
-        val=$(cat tmp.txt | grep "1 Time" | awk '{print $4}')
+        ./day$i ../input/day$i.txt > ./tmp1.txt
+        val=$(cat tmp1.txt | grep "1 Time" | awk '{print $4}')
         part1avg=$((val + part1avg))
         if [ $val -lt $part1best ] || [ $part1best -eq "0" ]; then
             part1best=$val
         fi
-        val=$(cat tmp.txt | grep "2 Time" | awk '{print $4}')
+        val=$(cat tmp1.txt | grep "2 Time" | awk '{print $4}')
         part2avg=$((val + part2avg))
         if [ $val -lt $part2best ] || [ $part2best -eq "0" ]; then
             part2best=$val
@@ -28,13 +30,12 @@ do
     done
     part1avg=$((part1avg / 100))
     part2avg=$((part2avg / 100))
-    echo -e "\tPart 1: \t$part1avg us \t\t$part1best us"
-    if [ $i -eq "6" ]; then
-        echo -e "\tPart 2: \t$part2avg us \t$part2best us"
-    else
-        echo -e "\tPart 2: \t$part2avg us \t\t$part2best us"
-    fi
+    echo " ;Part 1:;$part1avg us;$part1best us" >> tmp2.txt
+    echo " ;Part 2:;$part2avg us;$part2best us" >> tmp2.txt
 done
 
-rm tmp.txt
+echo -e "\r\e[K"
+column -t -s ";" tmp2.txt
+
+rm tmp1.txt tmp2.txt
 # make clean
